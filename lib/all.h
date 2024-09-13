cb634c02f75716c017a286253071f5d35639af9c
SysFugy <149673181+SysFugy@users.noreply.github.com>
2024-09-13 21:34:17 +0300

    Create all.h

---
+++ lib/all.h
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////
+//                                            //
+//                   all.h                    //
+//          All libaries in 1 file            //
+//                                            //
+////////////////////////////////////////////////
+
+#ifndef ALL_H
+#define ALL_H
+
+#include "acpi.h"
+#include "io.h"
+#include "string.h"
+#include "time.h"
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "msg.h"
+#include "check_drivers.h"
+
+#endif 
