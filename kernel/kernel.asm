2187f42fafacb05440a9ea06880522dca40a84c5
SysFugy <149673181+SysFugy@users.noreply.github.com>
2024-09-13 21:32:55 +0300

    Add files via upload

---
+++ kernel/kernel.asm
@@ -0,0 +1,28 @@
+
+;                    kernel.asm                 
+;	            Initialization              
+
+bits 32
+section .text
+        align 4
+        dd 0x1BADB002              	
+        dd 0x00                    	
+        dd - (0x1BADB002 + 0x00)   	
+
+global port_byte_in
+global start
+extern kmain 				
+
+port_byte_in:
+	mov dx, word [esp + 4]
+	in al, dx
+	ret
+start:
+	cli 				
+	mov esp, stack_space		
+	call kmain
+	hlt 				
+
+section .bss
+resb 8192
+stack_space:
---
+++ kernel/kernel.c
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////
+//                                            //
+//                  kernel.c                  //
+//	           Main file                  //
+//                                            //
+////////////////////////////////////////////////
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+#include "cfg.c"
+#include "../lib/all.h"
+
+#include "../progs/init/init.c"
+
+void kmain(void) {
+	init(0);	// Initialize IDT
+	init(1);	// Prepare
+	init(2);	// Logo
+	init(3);	// Logs
+	init(4);	// LLShell
+	
+	return;
+}
